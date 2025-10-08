70ee23aef6060dd6a921e6deeca1bea98195bc6b
Lucas Gomez <lukasg0396@gmail.com>
2025-10-08 17:17:09 -0300

    Update soporte.js
    Se actualizo el archivo

--- proyecto/assets/js/soporte.js
+++ proyecto/assets/js/soporte.js
@@ -32,4 +32,19 @@ document.addEventListener('DOMContentLoaded', function() {
             mensajeExito.classList.remove('mostrar');
         }, 5000);
     });
+    if (btnVolver) {
+        const params = new URLSearchParams(window.location.search);
+        const from = params.get("from");
+
+        btnVolver.addEventListener("click", () => {
+            if (from === "login") {
+                window.location.href = "../acceso/login.html";
+            } else if (from === "dashboard") {
+                window.location.href = "../admin/dashboard.html";
+            } else {
+                window.history.back(); // fallback
+            }
+        });
+    }
 });
+
