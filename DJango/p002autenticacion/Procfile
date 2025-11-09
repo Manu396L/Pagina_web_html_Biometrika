10bcd1961e796b8e6e5fd94511d5fd3f155a198d
Daniel <dz09dz@hotmail.com>
2025-11-09 11:10:36 -0300

    Configuraciones para Render

---
+++ DJango/p002autenticacion/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn p002autenticacion.wsgi
--- DJango/p002autenticacion/p002autenticacion/settings.py
+++ DJango/p002autenticacion/p002autenticacion/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!j%&##4smtolf)rzd$)q1o+^dc@@caz8g#lc3%1^0d39w_l+ed"
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'clave-temporal')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -123,6 +126,9 @@ STATICFILES_DIRS = [
 
 # En producción, los archivos estáticos se recolectan aquí
 STATIC_ROOT = BASE_DIR / "staticfiles"  # Esta es la carpeta donde 'collectstatic' pondrá los archivos estáticos
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
